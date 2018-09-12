f7c05c34455171b6e33f7dc824e9e1fccffd82b5
Urooj Ayub <uroojayub17@gmail.com>
2018-09-12 12:48:48 +0500

    Auth state listener added

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,2 @@
 export { addToCart,  removeFromCart, createNewOrder, clearSuccessError } from './shop-actions';
-export { signIn, signUp, signOut } from './auth-actions';
+export { signIn, signUp, signOut, observeAuthState} from './auth-actions';
